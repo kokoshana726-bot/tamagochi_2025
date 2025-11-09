8c99d20a46ab6589d64f9517a0e0796ec6d3eb5f
anna424 <72481727+anna424@users.noreply.github.com>
2025-11-09 22:49:31 +0900

    We made a list

---
+++ Lists.py
@@ -0,0 +1,9 @@
+l = [90, 1231, 20, 76, 34]
+
+for i in l:
+    print(f"The number to check is: {i}")
+    if i > 1000:
+        print("I'm a big number!")
+    else:
+        print("I'm a small number!")
+    print("")
